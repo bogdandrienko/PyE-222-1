441ddf9e9b2d466b9dade3f4a7d829c6c4eae9b3
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-11-18 20:23:54 +0600

    update

---
+++ projects/1/1_data_types.py
@@ -0,0 +1,13 @@
+bool1 = True
+int1 = 12
+float1 = 12.0
+str1 = "Python"
+list1 = [10, True, []]
+tuple1 = (12, False)
+set1 = {12, False}
+dict1 = {"name": "Bogdan", "age": 25}
+
+# val1 = 12
+# print(val1)
+# val1 = "Привет"
+# print(val1)
