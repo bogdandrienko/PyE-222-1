f7f6fe581c3638b27e93e3e9c324b3fcbdba2607
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2023-04-08 13:55:25 +0600

    update

--- projects/7/exam/main.py
+++ projects/7/exam/main.py
@@ -214,3 +214,233 @@ class LoginRequiredMixin:
 
 class Child1(Parent2, Parent1, LoginRequiredMixin):
     pass
+
+
+######################################################################################################################################################
+
+
+import math
+
+# 1.  В чем разница между списком и кортежем?
+# Элементы кортежа неизменяемы
+
+
+# 2.Как развернуть список?
+# 1) reverse
+arr = [1, 2, 3, 4, 5]
+arr.reverse()
+print(arr)
+# 2) slice
+arr1 = [1, 2, 3, 4, 5]
+print(arr1[::-1])
+
+# 3.Как округлить число до трех десятичных знаков?
+# 1)
+a = 2378.4687665
+print(round(a, 3))
+
+# 2)
+num = 3.14159265359
+formatted_num = "{:.3f}".format(num)
+print(formatted_num)
+# 3)
+print(math.floor(3.999))
+
+# 4)
+print(math.ceil(3.999))
+# 5)
+print(math.trunc(7.11))
+
+print(int(math.floor(2.6)))
+
+# 4.  Как получить абсолютное значение целого числа?
+# 1)
+x = -5
+abs_x = abs(x)
+print(abs_x)
+
+# 2)
+b = -6
+int_b = int(b)
+if int_b < 0:
+    int_b = int_b * -1
+else:
+    pass
+print(int_b)
+
+# 5.  Как перевести строку в верхний/нижний регистр?
+# 1)ВЕРХНИЙ РЕГИСТР
+string = "строка"
+upper_string = string.upper()
+print(upper_string)
+# 2)НИЖНИЙ РЕГИСТР
+string1 = "СТРОКА"
+lower_string = string.lower()
+print(lower_string)
+
+# 6.  Какие циклы есть в python и чем отличаются
+# 1) FOR
+for i in range(1, 10):
+    print(i)
+# 2) WHILE
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+# 3) DO WHILE
+c = 0
+while True:
+    print(c)
+    c += 1
+    if c == 10:
+        break
+
+# 7.  Строка — это последовательность или нет?
+# Да, строка является последовательностью символов, имеющих собственный индекс
+
+
+# 8.Что такое PEP?
+# PEP - это сокращение от "Python Enhancement Proposal" (предложение по улучшению Python).
+# PEP - это документ, который описывает и объясняет новую функциональность или улучшение языка Python.
+
+# 9.В чем разница между remove, del и pop?
+# remove
+my_list = [1, 2, 3, 2, 4, 5]
+my_list2 = [1, 2, 3, 2, 4, 5]
+
+print(my_list.remove(3))
+print(my_list)
+
+# del
+my_list2 = [1, 2, 3, 2, 4, 5]
+del my_list2[2]
+print(my_list2)
+
+# pop
+my_list3 = [1, 2, 3, 2, 4, 5]
+print(my_list3.pop(4))
+print(my_list3)
+
+# 10.Что такое лямбда-функция?
+# лямба-функция-это анонимная функция
+# ЛЯМБДА-ФУНКЦИЯ
+ab = lambda x: x ** 2
+abb = ab(5)
+print(abb)
+
+
+# ОБЫЧНАЯ ФУНКЦИЯ
+def square(a):
+    return a ** 2
+
+
+print(square(3))
+
+
+# 11.Магические методы и пару примеров
+class Class():
+    def __init__(self, value):
+        self.value = value
+
+    def __len__(self):
+        return len(str(self.value))
+
+
+######################################################################################################################################################
+
+
+# """11.  Проверьте, что в строке только числа"""
+#
+# # str1 = '12334324'
+# # str2 = 'fh2dg1jhj'
+#
+# def check(strr):
+#     for item in strr:
+#         if not item.isdigit():
+#             return False
+#     return
+#
+# # print(check(str1))
+# # print(check(str2))
+# #
+# # print(str1.isnumeric())
+# # print(str2.isnumeric())
+#
+# """21.  Что делает метод split() и join()"""
+#
+#
+# str1 = '123 343 24'
+#
+# #print(str1.split(" "))
+# # print(str2.join(""))
+#
+# myTuple = ("John", "Peter", "Vicky")
+# x = "#".join(myTuple)
+#
+# #print(x)
+#
+#
+#
+# """Как избежать конфликтов при импорте файлов"""
+#
+# from tkinter import ttk
+#
+# from json import *
+#
+# import pandas as pd
+#
+# import pandas, time
+#
+# from json import loads
+#
+#
+#
+#
+#
+# """48.  Как удалить из списка дубликаты?"""
+#
+# mylist = ["a", "b", "a", "c", "c"]
+# mylist2 = list(set(mylist))
+# # print(mylist2)
+#
+#
+# """49.  Как отсортировать словарь по ключам, в алфавитном порядке?"""
+#
+# footballers_goals = {'Eusebio': 120, 'Cruyff': 104, 'Pele': 150, 'Ronaldo': 132, 'Messi': 125}
+#
+# sorted_footballers_by_goals = sorted(footballers_goals.items(), key=lambda x:x[0])
+#
+# new_dict = dict((x, y) for x, y in sorted_footballers_by_goals)
+#
+# nw_dict = {}
+#
+# for x, y in sorted_footballers_by_goals:
+#     nw_dict[x] = y
+#
+# print(footballers_goals)
+# print(new_dict)
+# print(nw_dict)
+#
+
+
+# """"53.  Что такое Git и как создать гит репозиторий локально""" - git init
+
+# """67.  Где быстрее поиск: в словарях или списках? И почему."""
+# # dict > list
+# dict = O(1) const
+# list = O(n) lineiaer
+#
+"""76.  Напишите лучший код для перестановки двух чисел местами."""
+a = 9
+b = 7
+
+a, b = b, a
+
+
+
+
+
+
+
+
---
+++ projects/interview/requirements.py
@@ -0,0 +1,73 @@
+def operation_systems():
+    """
+OS Linux (CentOS, Debian, Ubuntu)
+Windows Server
+"""
+
+
+def databases():
+    """
+SQL: PostgreSQL, Oracle, MySQL, MSSQL,
+NOSQL: Redis, MongoDB, Kafka, Rabbitmq, Riak, RabbitMQ, ActiveMQ, MemCache, Scylla, Tarantool
+
+ORM
+ElasticSearch
+"""
+
+
+def addition():
+    """
+Знание Git, GitLab, Team City
+систем виртуализации (ESXi, Hyper-V, Proxmox, VMware, Virtualbox)
+Опыт работы с Docker, docker-compose, Kubernetes и контейнеризированными приложениями
+Уверенное знание Telegram API
+nginx, uWSGI
+ООП и следование Clean Code (PEP8, DRY, чистые функции и т.д.)
+Опыт написания Unit и интеграционных тестов
+
+Знание английского языка на уровне достаточном для переписки по техническим вопросам
+
+Опыт работы с микросервисной архитектурой
+алгоритмы, структуры данных, паттерны и принципы проектирования
+Task-трекерах: jira / trello / confluence
+JSON, CSV, XML
+
+SOAP, Web services и REST, SOLID, MVC
+"""
+
+
+def languages():
+    """
+Python
+Bash/Sh
+
+Go
+JavaScript
+ReactJS
+NodeJS
+html/css (slim, sass)
+"""
+
+
+def libraries():
+    """
+NumPy, SciPy, Pandas, scikit-learn;
+PyTorch, Hugging Face для сеток;
+Django, Django DRF, FastAPI, Flask, aiohttp, Tornado, Sanic
+SQLAlchemy
+WebSockets
+pandas, numpy, SciPy, Seaborn/Matplotlib
+gunicorn
+aioredis, asyncio, threading, multiprocessing, framework Trio, aiopg, psycopg
+pytorch, TF, keras, asure,
+Celery
+
+Hadoop, Spark, Greenplum, Data Lake, PySpark
+Selenium
+"""
+
+
+def f1():
+    a = """
+
+"""
